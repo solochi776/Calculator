6b4223c282173d8ac112f430cfd41a5c130b1ad1
vee <vanessahabasune@gmail.com>
2025-11-11 12:53:07 +0200

    i have add power,square_root,sine,cosine,tanget,logarithm to the project.

--- utils/adOps.py
+++ utils/adOps.py
@@ -1,1 +1,13 @@
 #advanced operators like square roots
+def power(x,y):
+    return x ** y
+def squre_root(x):
+    return math.sqrt(x)
+def sine(x):
+    return math.sin(math.radians(x))
+def cosine(x):
+    return math.cos(math.radians(x))
+def tanget(x):
+    return math.tan(math.radians(x))
+def logarithm(x, base=10):
+    return math.log(x, base)
